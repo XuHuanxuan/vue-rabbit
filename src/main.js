b35e19ee17d600354465a9c8535cd34b53c3100b
XuHuanxuan <xuhuanxuan4225@gmail.com>
2025-02-05 20:08:26 +0800

    axios 基础封装

--- src/main.js
+++ src/main.js
@@ -6,6 +6,11 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import { get } from '@/apis/testAPI.js'
+get().then(res => {
+    console.log(res)
+})
+
 const app = createApp(App)
 
 app.use(createPinia())
