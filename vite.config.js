5b25e5c50a9f5ee3302ad4540d4b3b877f115d23
XuHuanxuan <xuhuanxuan4225@gmail.com>
2025-02-05 19:42:20 +0800

    Element UI 引入

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
 
+// Element Plus 自动导入
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -9,6 +14,12 @@ export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    })
   ],
   resolve: {
     alias: {
