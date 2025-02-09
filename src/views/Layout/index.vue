6ecabe5e5a80039a497261d98c6dd46c84fc9b19
XuHuanxuan <xuhuanxuan4225@gmail.com>
2025-02-09 21:41:51 +0800

    Layout静态结构搭建

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,8 +1,12 @@
-<script>
-
+<script setup>
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
 </script>
 
 <template>
-    <div>index</div>
+    <LayoutNav />
+    <LayoutHeader />
     <RouterView />
+    <LayoutFooter />
 </template>
