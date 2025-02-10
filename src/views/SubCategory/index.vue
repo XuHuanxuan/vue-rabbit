4b09a6b739dd9e41e8612df2a54144fb36237434
XuHuanxuan <xuhuanxuan4225@gmail.com>
2025-02-10 20:54:17 +0800

    二级分类列表无限滚动

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -34,9 +34,21 @@ onMounted(() => {
 })
 
 const tabChange = () => {
-    console.log('tab切换了', requestData.value.sortField)
+    // console.log('tab切换了', requestData.value.sortField)
     requestData.value.page = 1
     getSubCategory()
+}
+
+const disabled = ref(false)
+const infiniteScroll = async () => {
+    // 获取下一页的数据
+    requestData.value.page++
+    const res = await axiosGetSubCategory(requestData.value)
+    goodsList.value = [...goodsList.value, ...res.data.result.items]
+    // 加载完毕 停止监听
+    if (res.data.result.items.length === 0) {
+        disabled.value = true
+    }
 }
 </script>
 
@@ -57,7 +69,7 @@ const tabChange = () => {
                 <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
                 <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
             </el-tabs>
-            <div class="body">
+            <div class="body" v-infinite-scroll="infiniteScroll" :infinite-scroll-disabled="disabled">
                 <!-- 商品列表-->
                 <GoodsItem v-for="item in goodsList" :goods="item" :key="item.id" />
             </div>
