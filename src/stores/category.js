7e02687efdb520ec0db73d1f1a8458251a6f379d
XuHuanxuan <xuhuanxuan4225@gmail.com>
2025-02-10 14:03:16 +0800

    Bug Fix

--- src/stores/category.js
+++ src/stores/category.js
@@ -1,6 +1,6 @@
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
-import { getCategoryAPI } from '@/apis/layout'
+import { axiosGetCategory } from '@/apis/layout'
 export const useCategoryStore = defineStore('category', () => {
     // 导航列表的数据管理
     // state 导航列表数据
@@ -8,7 +8,7 @@ export const useCategoryStore = defineStore('category', () => {
 
     // action 获取导航数据的方法
     const getCategory = async () => {
-        const res = await getCategoryAPI()
+        const res = await axiosGetCategory()
         categoryList.value = res.result
     }
 
