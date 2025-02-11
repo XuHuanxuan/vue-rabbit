f0261c4b270650eb6105ad1e9c76abc4489e8b43
XuHuanxuan <xuhuanxuan4225@gmail.com>
2025-02-11 21:10:13 +0800

    自定义校验

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -18,8 +18,8 @@ const rules = {
     ],
     agree: [
         {
-            validator: (rule, val, callback) => {
-                return val ? callback() : new Error('请先同意协议')
+            validator: (rule, value, callback) => {
+                return value ? callback() : callback(new Error('请先同意协议'))
             }
         }
     ]
@@ -55,8 +55,8 @@ const rules = {
                             <el-form-item prop="password" label="密码">
                                 <el-input v-model="userInfo.password" />
                             </el-form-item>
-                            <el-form-item label-width="22px">
-                                <el-checkbox size="large">
+                            <el-form-item prop="agree" label-width="22px">
+                                <el-checkbox size="large" v-model="userInfo.agree">
                                     我已同意隐私条款和服务条款
                                 </el-checkbox>
                             </el-form-item>
