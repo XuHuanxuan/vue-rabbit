229eb7b3e00679efef2c91954dea81f178c9169f
XuHuanxuan <xuhuanxuan4225@gmail.com>
2025-02-11 11:47:15 +0800

    详情页Sku组件

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -4,6 +4,7 @@ import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
 import DetailHot from './components/DetailHot.vue'
 import ImageView from '@/components/ImageView/index.vue'
+import XtxSku from '@/components/XtxSku/index.vue'
 
 const goods = ref({})
 const route = useRoute()
@@ -16,6 +17,12 @@ const getGoodDetail = async () => {
 onMounted(() => {
     getGoodDetail()
 })
+
+// skuChange
+const skuChange = (sku) => {
+    console.log(sku);
+    
+}
 </script>
 
 <template>
@@ -86,7 +93,7 @@ onMounted(() => {
                                 </dl>
                             </div>
                             <!-- sku组件 -->
-
+                            <XtxSku :goods="goods" @change="skuChange"></XtxSku>
                             <!-- 数据组件 -->
 
                             <!-- 按钮组件 -->
