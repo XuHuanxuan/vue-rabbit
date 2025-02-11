360935916608e4992f6104ecf8cd40069262ffd8
XuHuanxuan <xuhuanxuan4225@gmail.com>
2025-02-11 22:41:22 +0800

    token在拦截器中携带

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,5 +1,6 @@
 import axios from "axios"
 import { ElMessage } from 'element-plus'
+import { useUserStore } from "@/stores/user"
 import 'element-plus/theme-chalk/el-message.css'
 
 const httpInstance = axios.create({
@@ -10,7 +11,14 @@ const httpInstance = axios.create({
 // 添加请求拦截器
 httpInstance.interceptors.request.use(function (config) {
     // 在发送请求之前做些什么
-    return config;
+    // 1. 从pinia获取token数据
+    const userStore = useUserStore()
+    // 2. 按照后端的要求拼接token数据
+    const token = userStore.userInfo.token
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
 }, function (error) {
     // 对请求错误做些什么
     return Promise.reject(error);
